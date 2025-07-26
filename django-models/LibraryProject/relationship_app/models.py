724aa780408efac83ed0a643a806b82e3a2c5a0f
Hafsa <hafsarahoui9@gmail.com>
2025-07-26 05:49:40 +0200

    commit

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -10,9 +10,11 @@ class UserProfile(models.Model):
         ('Librarian', 'Librarian'),
         ('Member', 'Member'),
     ]
-
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
