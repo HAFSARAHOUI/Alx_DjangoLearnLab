1278aa3f0a75cb66dda448f7863f5386bded3b2a
Hafsa <hafsarahoui9@gmail.com>
2025-07-26 04:01:54 +0200

    commit

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from relationship_app.views import list_books
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('',include('bookshelf.urls')),
+    path('', include('relationship_app.urls')),
+    path('', list_books, name='home'),
+    path('books/', list_books, name='list_books'),
 ]
