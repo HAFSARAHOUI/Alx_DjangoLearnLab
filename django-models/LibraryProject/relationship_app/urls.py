2a81773e9c06ce3c57b0b67f4e672e2a64d98e9d
Hafsa <hafsarahoui9@gmail.com>
2025-07-26 04:58:14 +0200

    commit1

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
 from .views import CustomLoginView, CustomLogoutView, register
-
+from .views import register
 
 urlpatterns = [
     path('books/', list_books, name='list_books'),
